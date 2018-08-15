4fc606109d8bed887f1a8d520d5cbd3d8fe31f74
valentino <xvalentino13@gmail.com>
2018-08-15 00:25:16 -0600

    do some es5 stuff

--- dist/src.e23a1129.js
+++ dist/src.e23a1129.js
@@ -63717,7 +63717,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54624' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '60785' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
